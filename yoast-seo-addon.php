ab2b0aa48c77e5bde840c58be7de957536c3469d
Maksym Tsypliakov <makstsiplyakov@gmail.com>
2020-04-25 15:33:53 +0200

    Follow PHP Coding Standards

--- yoast-seo-addon.php
+++ yoast-seo-addon.php
@@ -20,10 +20,10 @@ final class Yoast_Seo_Add_On {
     protected static $instance;
 
     /**
-     * AddOn instance.
+     * Add On instance.
      * @var RapidAddon
      */
-    protected $addOn;
+    protected $add_on;
 
     /**
      * Return singletone instance.
@@ -40,12 +40,12 @@ final class Yoast_Seo_Add_On {
      * Yoast_Seo_Add_On constructor.
      */
     protected function __construct() {
-        $this->addOn = new RapidAddon( 'Yoast SEO Add-On', 'yoast_seo_addon' );
-        $this->addOn->add_field( 'yoast_wpseo_title', 'SEO Title', 'text' );
-        $this->addOn->add_field( 'yoast_wpseo_metadesc', 'Meta Description', 'text' );
-        $this->addOn->add_field( 'yoast_wpseo_meta-robots-noindex', 'Meta Robots Index', 'radio', ['' => 'default', '1' => 'noindex', '2' => 'index'] );
-        $this->addOn->add_field( 'yoast_wpseo_opengraph-image', 'Facebook Image', 'image' );
-        $this->addOn->set_import_function( [ $this, 'import' ] );
+        $this->add_on = new RapidAddon( 'Yoast SEO Add-On', 'yoast_seo_addon' );
+        $this->add_on->add_field( 'yoast_wpseo_title', 'SEO Title', 'text' );
+        $this->add_on->add_field( 'yoast_wpseo_metadesc', 'Meta Description', 'text' );
+        $this->add_on->add_field( 'yoast_wpseo_meta-robots-noindex', 'Meta Robots Index', 'radio', ['' => 'default', '1' => 'noindex', '2' => 'index'] );
+        $this->add_on->add_field( 'yoast_wpseo_opengraph-image', 'Facebook Image', 'image' );
+        $this->add_on->set_import_function( [ $this, 'import' ] );
         add_action( 'admin_init', [ $this, 'admin_init' ] );
     }
 
@@ -57,13 +57,13 @@ final class Yoast_Seo_Add_On {
             // Display this notice if neither the free or pro version of the Yoast plugin is active.
             if ( ! is_plugin_active( 'wordpress-seo/wp-seo.php' ) && ! is_plugin_active( 'wordpress-seo-premium/wp-seo-premium.php' ) ) {
                 // Specify a custom admin notice.
-                $this->addOn->admin_notice(
+                $this->add_on->admin_notice(
                     'The Yoast WordPress SEO Add-On requires WP All Import <a href="http://wordpress.org/plugins/wp-all-import" target="_blank">Free</a> and the <a href="https://yoast.com/wordpress/plugins/seo/">Yoast WordPress SEO</a> plugin.'
                 );
             }
             // Only run this add-on if the free or pro version of the Yoast plugin is active.
             if ( is_plugin_active( 'wordpress-seo/wp-seo.php' ) || is_plugin_active( 'wordpress-seo-premium/wp-seo-premium.php' ) ) {
-                $this->addOn->run();
+                $this->add_on->run();
             }
         }
     }
@@ -77,19 +77,19 @@ final class Yoast_Seo_Add_On {
      */
     public function import( $post_id, $data, $import_options ) {
 
-        if ( $this->addOn->can_update_meta( '_yoast_wpseo_title', $import_options ) ) {
+        if ( $this->add_on->can_update_meta( '_yoast_wpseo_title', $import_options ) ) {
             update_post_meta( $post_id, '_yoast_wpseo_title', $data['yoast_wpseo_title'] );
         }
 
-        if ( $this->addOn->can_update_meta( '_yoast_wpseo_metadesc', $import_options ) ) {
+        if ( $this->add_on->can_update_meta( '_yoast_wpseo_metadesc', $import_options ) ) {
             update_post_meta( $post_id, '_yoast_wpseo_metadesc', $data['yoast_wpseo_metadesc'] );
         }
 
-        if ( $this->addOn->can_update_meta( '_yoast_wpseo_meta-robots-noindex', $import_options ) ) {
+        if ( $this->add_on->can_update_meta( '_yoast_wpseo_meta-robots-noindex', $import_options ) ) {
             update_post_meta( $post_id, '_yoast_wpseo_meta-robots-noindex', $data['yoast_wpseo_meta-robots-noindex'] );
         }
 
-        if ( $this->addOn->can_update_image( $import_options ) ) {
+        if ( $this->add_on->can_update_image( $import_options ) ) {
             $image_url = wp_get_attachment_url( $data['yoast_wpseo_opengraph-image']['attachment_id'] );
             update_post_meta( $post_id, '_yoast_wpseo_opengraph-image', $image_url );
         }
